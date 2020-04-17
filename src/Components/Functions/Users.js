65975809152fe6e71f47c1e8cc94246bdff9d128
vj abishek <abigosearch@gmail.com>
2020-04-17 19:48:14 +0530

    Changing the server URL to heroku

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -3,6 +3,6 @@ import io from 'socket.io-client'
 const socket =
   window.location.host === 'localhost:3000'
     ? io.connect('localhost:3030')
-    : io.connect('https://airdrop-server.now.sh/')
+    : io.connect('https://abigo-facetime.herokuapp.com/')
 
 export default socket
