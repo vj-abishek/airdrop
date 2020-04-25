31cb60b5f0d2d6dbd2adba37e8d8a351833106e5
Jack-in <64306586+Jack-in@users.noreply.github.com>
2020-04-25 16:06:16 +0530

    Update Article.jsx

--- src/Components/Section/Article.jsx
+++ src/Components/Section/Article.jsx
@@ -16,14 +16,15 @@ export default function Article() {
           <br />
           <h4>Security</h4>
           <p>
-            Share your files privately without being leaked . This is a realtime
-            sharing system. The files you shared won't be uploaded to our
+            Share your files privately without getting them leaked . This is a realtime
+            sharing system. The files you share won't be uploaded to our
             server. This website is based on
             <a href='https://en.wikipedia.org/wiki/WebRTC' target='blank'>
               WEBRTC
             </a>
             . A realtime communication channel to transfer data. You can
-            transfer your files to anyone in the world without tension
+            transfer your files to anyone, anywhere in the world without 
+            Worrying about confidentiality. 
           </p>
         </div>
 
@@ -36,10 +37,10 @@ export default function Article() {
           </i>
           <h4>Communication</h4>
           <p>
-            Now you can communicate with the other peer while sending files.
-            Your chat is also private and you can also use our website for
-            secrete conversation. This is a handy feature which enables you to
-            talk with each other and share your desired files
+            Now you can communicate with the other peer along with sending files.
+            Your chat is private and you also can use our website for
+            secrete conversations. This is a handy feature which enables you to
+            chat with each other and share your desired files
           </p>
         </div>
         <div>
@@ -52,8 +53,8 @@ export default function Article() {
           <h4>Easy to use</h4>
           <p>
             This website is easy to use. Share with one tap. Share your desired
-            files with anyone around the world. Share your files with 3 simple
-            steps. Easy as a breeze
+            files with anyone anywhere in the world. Start sharing your files after 3 simple
+            steps. Easy as a breeze!
           </p>
         </div>
       </div>
