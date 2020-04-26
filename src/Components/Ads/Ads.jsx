7b9f1e6c5034717c5648da8f9d076ac2f5dcd969
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-04-26 18:56:14 +0530

    Adding google ads

--- src/Components/Ads/Ads.jsx
+++ src/Components/Ads/Ads.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default class Ad extends React.Component {
   componentDidMount() {
-    (window.adsbygoogle = window.adsbygoogle || []).push({})
+    if (window) (window.adsbygoogle = window.adsbygoogle || []).push({})
   }
 
   render() {
@@ -10,19 +10,30 @@ export default class Ad extends React.Component {
       <div
         style={{
           position: 'fixed',
-          width: '100%',
+          width: '970px',
+          height: ' 90px',
           bottom: '0px',
-
         }}
       >
         <ins
           className='adsbygoogle'
-          style={{ display: 'block' }}
+          style={{ display: 'block', width: '100%', height: ' 50px' }}
           data-ad-client='ca-pub-4314629461501774'
           data-ad-slot='5177790706'
           data-ad-format='auto'
           data-full-width-responsive='true'
         ></ins>
+        <p
+          style={{
+            textAlign: 'center',
+            margin: 'auto',
+            position: 'relative',
+            zIndex: -1,
+          }}
+        >
+          Please disable ad-blocker! You may not like this ad, but it supports
+          the developer and keeps this site free.
+        </p>
       </div>
     )
   }
