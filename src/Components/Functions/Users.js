394932b518a73078793187002539931cae2ef0eb
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-01 21:18:32 +0530

    Changed the server URL to now.sh

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -3,6 +3,6 @@ import io from 'socket.io-client'
 const socket =
   window.location.host === 'localhost:3000'
     ? io.connect('localhost:3030')
-    : io.connect('https://abigo-facetime.herokuapp.com/')
+    : io.connect('https://server.safeshare.live/')
 
 export default socket
