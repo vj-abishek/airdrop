fb0164f954ac34b5240c22327e68745c1a20a6e1
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-04 15:05:50 +0530

    Bug Fixes:

--- src/Tools/Getdevice.js
+++ src/Tools/Getdevice.js
@@ -1,9 +1,7 @@
 const device = () => {
   const UA = navigator.userAgent
-  const another = UA.split('))')
-  const any = another[0].split('(')
-  const final = any[1].split(';')
-  return final
+  UA.split(';')
+  return UA
 }
 
 export default device
