4fa4482f44ec745d79893977be5a1a663f882444
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-05 12:40:02 +0530

    Testing web share target

--- src/Components/Share/Share.jsx
+++ src/Components/Share/Share.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Share(props) {
-  console.log(props)
-  return <div></div>
+export default function Share() {
+  const [text, setText] = useState()
+  useEffect(() => {
+    const parsedUrl = new URL(window.location)
+    // searchParams.get() will properly handle decoding the values.
+    console.log('Title shared: ' + parsedUrl.searchParams.get('title'))
+    console.log('Text shared: ' + parsedUrl.searchParams.get('text'))
+    console.log('URL shared: ' + parsedUrl.searchParams.get('url'))
+    setText(parsedUrl.searchParams.get('text'))
+  }, [])
+  return <div>{text}</div>
 }
