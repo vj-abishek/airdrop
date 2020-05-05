c3afc1093e7ccda55d8e2483fe698b8203d1ad6b
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-05 13:24:38 +0530

    Testing Web file share

--- src/Components/Share/Share.jsx
+++ src/Components/Share/Share.jsx
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from 'react'
 export default function Share() {
   const [text, setText] = useState()
   useEffect(() => {
+    navigator.serviceWorker.onmessage = (e) => {
+      console.log(e.data.file)
+      setText('Success in file')
+    }
     const parsedUrl = new URL(window.location)
     // searchParams.get() will properly handle decoding the values.
     console.log('Title shared: ' + parsedUrl.searchParams.get('title'))
