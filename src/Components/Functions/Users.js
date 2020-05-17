8de34715670bd3ca4e8f02d87e0f0708132efdcd
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-17 11:40:15 +0530

    Testing the server with firebase

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -3,6 +3,8 @@ import io from 'socket.io-client'
 const socket =
   window.location.host === 'localhost:3000'
     ? io.connect('localhost:3030')
-    : io.connect('https://abigo-facetime.herokuapp.com/')
+    : io.connect('https://us-central1-denoreact.cloudfunctions.net/app')
 
 export default socket
+
+// https://abigo-facetime.herokuapp.com/
