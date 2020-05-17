0f11fd245ecbd2610de7f3215013538ce33e1056
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-17 13:16:34 +0530

    Changing the url to heroku
    Because of firebase functions issues with CORS

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 const socket =
   window.location.host === 'localhost:3000'
     ? io.connect('localhost:3030')
-    : io.connect('https://denoreact.web.app/')
+    : io.connect('https://abigo-facetime.herokuapp.com/')
 
 export default socket
 
