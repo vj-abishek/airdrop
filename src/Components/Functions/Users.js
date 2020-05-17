67bd0d80e3f799d70c8438acc7b626734f4f0a93
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-17 13:10:29 +0530

    Testing server URL

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -3,8 +3,11 @@ import io from 'socket.io-client'
 const socket =
   window.location.host === 'localhost:3000'
     ? io.connect('localhost:3030')
-    : io.connect('https://denoreact.web.app/')
+    : io.connect('https://us-central1-denoreact.cloudfunctions.net/app')
 
 export default socket
 
+//The server URL
+//https://denoreact.web.app/
+//https://us-central1-denoreact.cloudfunctions.net/app
 // https://abigo-facetime.herokuapp.com/
