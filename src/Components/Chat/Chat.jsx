830f60ad54ab2c6fa477beaf72dcf24b69d544e5
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-05-25 10:55:43 +0530

    Added SEO for chat.jsx

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -3,7 +3,7 @@ import peer from './Peer'
 import { share_file, bufferArrayuni, dataURItoBlob } from './FileShare/File.js'
 import Success from './Success'
 import { v4 as uuid } from 'uuid'
-
+import { Helmet } from 'react-helmet'
 import { from } from 'rxjs'
 import { useParams } from 'react-router-dom'
 import './Chat.css'
@@ -263,155 +263,166 @@ export default function Chat() {
   }
 
   return (
-    <div className='container'>
-      <div className='chat-container'>
-        <div
-          style={{
-            display: 'flex',
-            width: '98%',
-            height: '30px',
-            padding: '10px',
-            background: '#181717',
-            color: '#fff',
-          }}
-        >
-          <a className=' Iazdo' href='/'>
-            <span
-              style={{ display: 'inline-block', transform: 'rotate(270deg)' }}
-            >
-              <svg
-                aria-label='Back'
-                className='_8-yf5 '
-                fill='#fff'
-                height='24'
-                viewBox='0 0 48 48'
-                width='24'
+    <>
+      <Helmet>
+        <title>Safeshare.live - Now transfer your files - {id}</title>
+        <link rel='canonical' href='https://safeshare.live/' />
+        <meta
+          name='description'
+          content='SafeShare.live is a online file sharing service. 1. Create a name. 2. Choose a person and send the file realtime'
+        />
+      </Helmet>
+      <div className='container'>
+        <div className='chat-container'>
+          <div
+            style={{
+              display: 'flex',
+              width: '98%',
+              height: '30px',
+              padding: '10px',
+              background: '#181717',
+              color: '#fff',
+            }}
+          >
+            <a className=' Iazdo' href='/'>
+              <span
+                style={{ display: 'inline-block', transform: 'rotate(270deg)' }}
               >
-                <path d='M40 33.5c-.4 0-.8-.1-1.1-.4L24 18.1l-14.9 15c-.6.6-1.5.6-2.1 0s-.6-1.5 0-2.1l16-16c.6-.6 1.5-.6 2.1 0l16 16c.6.6.6 1.5 0 2.1-.3.3-.7.4-1.1.4z'></path>
-              </svg>
-            </span>
-          </a>
-          <header> {id}</header>
-          {window.location.hash === '#init' ? (
-            <span className='msg-check'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 16 15'
-                width='16'
-                height='15'
-              >
-                <path
+                <svg
+                  aria-label='Back'
+                  className='_8-yf5 '
                   fill='#fff'
-                  d='M10.91 3.316l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z'
-                ></path>
-              </svg>
-            </span>
-          ) : (
-            ''
-          )}
-        </div>
-        <div className='Message'>
-          {connected ? (
-            message.length > 0 ? (
-              message.map((data, i) => {
-                let condition = /^image/gi.test(data.type)
-
-                return condition ? (
-                  <ImageCon
-                    key={data.id}
-                    data={data}
-                    messageContainer={messageContainer}
-                    imageID={imageID}
-                  />
-                ) : (
-                  <Message
-                    key={data.id}
-                    forwardedRef={messageContainer}
-                    data={data}
-                  />
-                )
-              })
+                  height='24'
+                  viewBox='0 0 48 48'
+                  width='24'
+                >
+                  <path d='M40 33.5c-.4 0-.8-.1-1.1-.4L24 18.1l-14.9 15c-.6.6-1.5.6-2.1 0s-.6-1.5 0-2.1l16-16c.6-.6 1.5-.6 2.1 0l16 16c.6.6.6 1.5 0 2.1-.3.3-.7.4-1.1.4z'></path>
+                </svg>
+              </span>
+            </a>
+            <header> {id}</header>
+            {window.location.hash === '#init' ? (
+              <span className='msg-check'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 16 15'
+                  width='16'
+                  height='15'
+                >
+                  <path
+                    fill='#fff'
+                    d='M10.91 3.316l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z'
+                  ></path>
+                </svg>
+              </span>
             ) : (
-              <Success />
-            )
-          ) : (
-            <p
-              style={{
-                color: '#fff',
-                textAlign: 'center',
-                verticalAlign: 'center',
-              }}
-            >
-              Setting everything up, Don't Refresh, Please wait...
-            </p>
-          )}
-        </div>
-        <div className='Message-text-box'>
-          <form onSubmit={handleSubmit}>
-            <div className='input-ka-name'>
-              <div style={{ flex: 1 }}>
-                <input
-                  style={{
-                    width: '99%',
-                    height: '30px',
-                    color: '#000',
-                    padding: '3px 10px 3px 24px',
-                    borderRadius: '40px 0 0 40px',
-                    outline: 'none',
-                    border: '0px',
-                  }}
-                  ref={inputVariable}
-                  type='text'
-                  onChange={handleChange}
-                  placeholder='Type a message or send a file... '
-                  autoFocus
-                  // style={{ width: '100%', height: '50px', outline: 'none' }}
-                />
-                <input
-                  type='file'
-                  ref={file}
-                  onChange={handleFileChange}
-                  style={{
-                    cursor: 'text',
-                    whiteSpace: 'pre-wrap',
-                    overflowWrap: 'break-word',
-                  }}
-                  hidden
-                />
-              </div>
-              <div>
-                <div className='button-style'>
-                  <button
-                    className='wpO6b '
+              ''
+            )}
+          </div>
+
+          <div className='Message'>
+            {connected ? (
+              message.length > 0 ? (
+                message.map((data, i) => {
+                  let condition = /^image/gi.test(data.type)
+
+                  return condition ? (
+                    <ImageCon
+                      key={data.id}
+                      data={data}
+                      messageContainer={messageContainer}
+                      imageID={imageID}
+                    />
+                  ) : (
+                    <Message
+                      key={data.id}
+                      forwardedRef={messageContainer}
+                      data={data}
+                    />
+                  )
+                })
+              ) : (
+                <Success />
+              )
+            ) : (
+              <p
+                style={{
+                  color: '#fff',
+                  textAlign: 'center',
+                  verticalAlign: 'center',
+                }}
+              >
+                Setting everything up, Don't Refresh, Please wait...
+              </p>
+            )}
+          </div>
+          <div className='Message-text-box'>
+            <form onSubmit={handleSubmit}>
+              <div className='input-ka-name'>
+                <div style={{ flex: 1 }}>
+                  <input
                     style={{
-                      background: 'transparent',
-                      border: '0px',
-                      cursor: 'pointer',
+                      width: '99%',
+                      height: '30px',
+                      color: '#000',
+                      padding: '3px 10px 3px 24px',
+                      borderRadius: '40px 0 0 40px',
                       outline: 'none',
+                      border: '0px',
                     }}
-                    type='button'
-                    onClick={handleFile}
-                  >
-                    <svg
-                      aria-label='Add Photo or Video'
-                      className='_8-yf5 '
-                      fill='#262626'
-                      height='24'
-                      viewBox='0 0 48 48'
-                      width='24'
+                    ref={inputVariable}
+                    type='text'
+                    onChange={handleChange}
+                    placeholder='Type a message or send a file... '
+                    autoFocus
+                    // style={{ width: '100%', height: '50px', outline: 'none' }}
+                  />
+                  <input
+                    type='file'
+                    ref={file}
+                    onChange={handleFileChange}
+                    style={{
+                      cursor: 'text',
+                      whiteSpace: 'pre-wrap',
+                      overflowWrap: 'break-word',
+                    }}
+                    hidden
+                  />
+                </div>
+                <div>
+                  <div className='button-style'>
+                    <button
+                      className='wpO6b '
+                      style={{
+                        background: 'transparent',
+                        border: '0px',
+                        cursor: 'pointer',
+                        outline: 'none',
+                      }}
+                      type='button'
+                      onClick={handleFile}
                     >
-                      <path d='M38.5 0h-29C4.3 0 0 4.3 0 9.5v29C0 43.7 4.3 48 9.5 48h29c5.2 0 9.5-4.3 9.5-9.5v-29C48 4.3 43.7 0 38.5 0zM45 38.5c0 3.6-2.9 6.5-6.5 6.5h-29c-3.3 0-6-2.5-6.4-5.6l8.3-8.3c.1-.1.3-.2.4-.2.1 0 .2 0 .4.2l6.3 6.3c1.4 1.4 3.6 1.4 5 0L35.9 25c.2-.2.6-.2.8 0l8.3 8.3v5.2zm0-9.4l-6.2-6.2c-1.3-1.3-3.7-1.3-5 0L21.3 35.3c-.1.1-.3.2-.4.2-.1 0-.2 0-.4-.2L14.2 29c-1.3-1.3-3.7-1.3-5 0L3 35.2V9.5C3 5.9 5.9 3 9.5 3h29C42.1 3 45 5.9 45 9.5v19.6zM11.8 8.2c-1.9 0-3.5 1.6-3.5 3.5s1.6 3.5 3.5 3.5 3.5-1.6 3.5-3.5-1.6-3.5-3.5-3.5z'></path>
-                    </svg>
-                  </button>
-                  <button className='sqdOP' type='submit'>
-                    Send
-                  </button>
+                      <svg
+                        aria-label='Add Photo or Video'
+                        className='_8-yf5 '
+                        fill='#262626'
+                        height='24'
+                        viewBox='0 0 48 48'
+                        width='24'
+                      >
+                        <path d='M38.5 0h-29C4.3 0 0 4.3 0 9.5v29C0 43.7 4.3 48 9.5 48h29c5.2 0 9.5-4.3 9.5-9.5v-29C48 4.3 43.7 0 38.5 0zM45 38.5c0 3.6-2.9 6.5-6.5 6.5h-29c-3.3 0-6-2.5-6.4-5.6l8.3-8.3c.1-.1.3-.2.4-.2.1 0 .2 0 .4.2l6.3 6.3c1.4 1.4 3.6 1.4 5 0L35.9 25c.2-.2.6-.2.8 0l8.3 8.3v5.2zm0-9.4l-6.2-6.2c-1.3-1.3-3.7-1.3-5 0L21.3 35.3c-.1.1-.3.2-.4.2-.1 0-.2 0-.4-.2L14.2 29c-1.3-1.3-3.7-1.3-5 0L3 35.2V9.5C3 5.9 5.9 3 9.5 3h29C42.1 3 45 5.9 45 9.5v19.6zM11.8 8.2c-1.9 0-3.5 1.6-3.5 3.5s1.6 3.5 3.5 3.5 3.5-1.6 3.5-3.5-1.6-3.5-3.5-3.5z'></path>
+                      </svg>
+                    </button>
+                    <button className='sqdOP' type='submit'>
+                      Send
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
