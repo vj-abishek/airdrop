3915d3232f0478947d67a4d193d319dc563c8434
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-07-12 16:29:22 +0530

    Some changes in share component

--- src/Components/Share/Share.jsx
+++ src/Components/Share/Share.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 export default function Share() {
-  const [text, setText] = useState()
+  const [text, setText] = useState();
   useEffect(() => {
     navigator.serviceWorker.onmessage = (e) => {
-      console.log(e.data.file)
-      setText('Success in file')
-    }
-    const parsedUrl = new URL(window.location)
+      console.log(e.data.file);
+      setText('Success in file');
+    };
+    const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
-    console.log('Title shared: ' + parsedUrl.searchParams.get('title'))
-    console.log('Text shared: ' + parsedUrl.searchParams.get('text'))
-    console.log('URL shared: ' + parsedUrl.searchParams.get('url'))
-    setText(parsedUrl.searchParams.get('text'))
-  }, [])
-  return <div>{text}</div>
+    console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+    console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+    setText(parsedUrl.searchParams.get('text'));
+  }, []);
+  return (
+    <h1 style={{ textAlign: 'center' }}>This is the sharing option{text}</h1>
+  );
 }
