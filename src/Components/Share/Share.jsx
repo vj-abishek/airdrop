f066c77b328f2d4eb9fc5123adb324cf4032dec9
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-07-12 17:31:35 +0530

    Test

--- src/Components/Share/Share.jsx
+++ src/Components/Share/Share.jsx
@@ -1,16 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 export default function Share() {
-  const [text, setText] = useState();
-  useEffect(() => {
-    navigator.serviceWorker.onmessage = (e) => {
-      const localFile = e.data.file;
-      console.log(localFile);
-      setText('Success in file');
-      alert('Success from share.jsx');
-    };
-  }, [text]);
-  return (
-    <h6 style={{ textAlign: 'center' }}>This is the sharing option{text}</h6>
-  );
+  return <h6>This is your image</h6>;
 }
