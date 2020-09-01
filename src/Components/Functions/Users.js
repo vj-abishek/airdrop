45b16a3d7dd1a09b78e1c750e8f67f4e7ed3f5c4
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-09-01 19:17:45 +0530

    🎉 Server finally working

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 
 // const socket = (window.location.hostname === 'localhost' || '192.168.43.50') ? io.connect(`http://${window.location.hostname}:3030`) : io.connect('https://abigo-facetime.herokuapp.com/', { transports: ['websocket'] })
 
-const socket = io.connect('https://abigo-facetime.herokuapp.com/', { transports: ['websocket'] })
+const socket = io.connect('https://abigo.educationhost.cloud/', { transports: ['websocket'] })
 
 export default socket
 
