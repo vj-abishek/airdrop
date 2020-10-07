13b4a14ea501ae75500e727ecff0fcc1060a70f8
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-10-07 15:35:49 +0530

    Bug fix

---
+++ src/config/fb.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: 'abigo-share.firebaseapp.com',
+    databaseURL: 'https://abigo-share.firebaseio.com',
+    projectId: 'abigo-share',
+    storageBucket: 'abigo-share.appspot.com',
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURMENT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+if (process.env.NODE_ENV === 'production') {
+    firebase.analytics();
+}
+
+export default firebase;
