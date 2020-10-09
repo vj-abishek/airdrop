49c870b1194b7611848be222b48ed45790f62918
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-10-09 16:10:07 +0530

    Add purgecss to remove unused twailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ],
+const devEnv = {
+    plugins: [require("tailwindcss"), require("autoprefixer")],
 };
+if (process.env.NODE_ENV === "production") {
+    devEnv.plugins.push(
+        require("@fullhuman/postcss-purgecss")({
+            content: ["./src/Components/**.jsx", "./src/Components/**.js"],
+            defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        })
+    );
+}
+module.exports = devEnv;
