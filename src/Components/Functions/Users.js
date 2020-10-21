6ab5a0ae529a8f8a6467a94d4298e3d3e131dcc3
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-10-21 16:09:19 +0530

    Change server url

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 const connectionString =
   window.location.hostname === 'localhost'
     ? `http://${window.location.hostname}:8080`
-    : 'https://relp-server.herokuapp.com/';
+    : 'https://relp-server1.herokuapp.com/';
 const socket = io.connect(connectionString);
 
 export default socket;
