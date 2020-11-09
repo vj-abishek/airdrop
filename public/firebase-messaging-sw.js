8acb402ea83103c29c3958c7500ac574397c8385
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-11-09 13:12:02 +0530

    Add PWA Icons

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,6 +6,57 @@
 importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js');
 
+// importScripts('https://unpkg.com/buffer@5.6.0/index.js');
+// importScript('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js');
+// importScripts('https://unpkg.com/dexie@latest/dist/dexie.js');
+
+
+// const db = new Dexie('user_database');
+// db.version(1).stores({
+//     channel: 'id',
+// });
+
+// const getKeys = async (channelId) => {
+//     return new Promise((res, rej) => {
+//         try {
+//             db.channel
+//                 .where('id')
+//                 .equalsIgnoreCase(channelId)
+//                 .toArray()
+//                 .then((key) => {
+//                     if (key) {
+//                         const { SharedSecret } = key[0];
+//                         res(SharedSecret);
+//                     }
+
+//                 });
+//         } catch (err) {
+//             rej(err);
+//         }
+//     });
+// }
+
+// const decrypt = async (SharedSecret, encryptedMessage) => {
+
+//     const bobPayload = Buffer.from(encryptedMessage, 'base64').toString('hex');
+
+//     const bobIv = bobPayload.substr(0, 32);
+//     const bobEncrypted = bobPayload.substr(32, bobPayload.length - 32 - 32);
+//     const bobAuthtag = bobPayload.substr(bobPayload.length - 32, 32);
+
+//     const Decipher = crypto.createDecipheriv(
+//         'aes-256-gcm',
+//         Buffer.from(SharedSecret, 'hex'),
+//         Buffer.from(bobIv, 'hex'),
+//     );
+
+//     Decipher.setAuthTag(Buffer.from(bobAuthtag, 'hex'));
+
+//     let decrypt = Decipher.update(bobEncrypted, 'hex', 'utf8');
+//     decrypt += Decipher.final('utf8');
+
+//     return decrypt;
+// }
 
 
 const firebaseConfig = {
@@ -24,23 +75,34 @@ firebase.initializeApp(firebaseConfig);
 // messages.
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function (payload) {
+messaging.setBackgroundMessageHandler(async function (payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     const parsed = JSON.parse(payload.data.body);
+
+
+    // try {
+    //     const SharedSecret = await getKeys(parsed.channel);
+    //     console.log(sharedSecret);
+    //     const decrypt = await decrypt(SharedSecret, payload.data.body);
+    //     console.log(decrypt);
+
+    // } catch (err) {
+    //     console.log(err);
+    // }
     // Customize notification here
     const notificationTitle = "New message on relp";
     const notificationOptions = {
         body: `You got a new message from ${parsed.displayName}`,
         icon: parsed.photoURL,
         vibrate: [300, 100, 400, 100, 400, 100, 400],
-        data: { url: `https://relp.now.sh/r/${parsed.channel}` },
+        data: { url: 'https://relp.now.sh/' },
         actions: [{ action: "open_url", title: "Read Message" }],
-        click_action: `https://relp.now.sh/r/${parsed.channel}`
+        click_action: 'https://relp.now.sh/'
     };
 
-    self.addEventListener('notificationclick', function(event) {
+    self.addEventListener('notificationclick', function (event) {
         event.notification.close();
-        event.waitUntil(self.clients.openWindow(`https://relp.now.sh/r/${parsed.channel}`));
+        event.waitUntil(self.clients.openWindow('https://relp.now.sh/'));
     });
 
     return self.registration.showNotification(notificationTitle, notificationOptions);
