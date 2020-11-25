62620c0beddd4f7d5500f1fe6edb9136807ef520
Abishek <43115551+vj-abishek@users.noreply.github.com>
2020-11-25 10:58:15 +0530

    v1.0.1

--- src/Components/About/Content/Build.jsx
+++ src/Components/About/Content/Build.jsx
@@ -5,7 +5,7 @@ export default function Build() {
   return (
     <Main>
       <div className="text-base">Build version</div>
-      <div className="text-light text-sm">v1.0.0</div>
+      <div className="text-light text-sm">v1.0.1</div>
     </Main>
   );
 }
