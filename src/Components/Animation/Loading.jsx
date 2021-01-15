8d6a625272d6312e271724b986d59e2e19c87112
Abishek <43115551+vj-abishek@users.noreply.github.com>
2021-01-15 10:40:38 +0530

    Add neomorphism effect for loading

--- src/Components/Animation/Loading.jsx
+++ src/Components/Animation/Loading.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-// import Logo from '../../assets/logo_512x512.svg';
+import Styles from '../../Styles/responsive.module.css';
+
+const matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 const Loading = () => (
   <div className="flex w-screen h-screen justify-center items-center bg-primary fixed z-50">
-    {/* <img className="w-24 h-24" src={Logo} alt="Loading..." /> */}
-    <svg
-      className="w-24 h-24"
-      viewBox="0 0 512 512"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M461.143 265C594.906 -107.264 71 512 71 512C71 512 146.242 401.414 146.242 265C461.143 55.05 71 18 71 18C71 18 327.379 637.264 461.143 265Z"
-        fill="#00B2D2"
-      />
-    </svg>
+    <div className={`${matched ? Styles.neomorphism : Styles.lightNeomorphism} p-16 `}>
+      <svg
+        className="w-24 h-24"
+        viewBox="0 0 512 512"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M461.143 265C594.906 -107.264 71 512 71 512C71 512 146.242 401.414 146.242 265C461.143 55.05 71 18 71 18C71 18 327.379 637.264 461.143 265Z"
+          fill="#00B2D2"
+        />
+      </svg>
+    </div>
+
   </div>
 );
 
