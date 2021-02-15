2437f6724d711c56997c90c5bdbd0c838c208369
Abishek <43115551+vj-abishek@users.noreply.github.com>
2021-02-15 12:37:49 +0530

    code refactor

--- src/Components/Utils/LastSeen.js
+++ src/Components/Utils/LastSeen.js
@@ -2,11 +2,11 @@ import { isToday, isYesterday, format } from 'date-fns';
 
 export default function LastSeen(date) {
     if (isToday(date)) {
-        return format(date, "'Last seen at' hh:mm a");
+        return format(date, "'last seen at' hh:mm a");
     }
     if (isYesterday(date)) {
-        return format(date, "'Last seen yesterday at' hh:mm a");
+        return format(date, "'last seen yesterday at' hh:mm a");
     }
 
-    return format(date, "'Last seen' MMM dd 'at' hh:mm a");
+    return format(date, "'last seen' MMM dd 'at' hh:mm a");
 }
