3cf4a5f5c3eff8ccd435e7beca30dbead520d410
Abishek <43115551+vj-abishek@users.noreply.github.com>
2021-04-03 17:59:50 +0530

    feat: changed server url

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -1,7 +1,6 @@
 import io from 'socket.io-client';
 
-const connectionString =
-  window.location.hostname === 'localhost'
+const connectionString = window.location.hostname === 'localhost'
     ? `http://${window.location.hostname}:8080`
     : 'https://relp.vsbabuj.repl.co';
 const socket = io.connect(connectionString);
