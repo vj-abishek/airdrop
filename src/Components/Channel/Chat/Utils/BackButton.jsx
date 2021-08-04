5b772815ca7a2bbafb307e77a58d5279d5f59c7d
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2021-08-04 15:43:29 +0530

    fix: back button issue 😢

--- src/Components/Channel/Chat/Utils/BackButton.jsx
+++ src/Components/Channel/Chat/Utils/BackButton.jsx
@@ -19,7 +19,7 @@ export default function BackButon() {
       onClick={handleClick}
       onKeyPress={handleClick}
     >
-      <svg viewBox="0 0 24 24" aria-hidden="true" class="fill-current text-accent">
+      <svg viewBox="0 0 24 24" aria-hidden="true" className="fill-current text-accent">
         <g>
            <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path>
         </g>
