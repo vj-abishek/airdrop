9d30c7c43b41ce7e7a6efc6a82a8ac1a15619caf
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2022-02-07 09:19:02 +0530

    Update Users.js

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -2,8 +2,10 @@ import io from 'socket.io-client';
 
 const connectionString = window.location.hostname === 'localhost'
     ? `http://${window.location.hostname}:8080`
-    : 'https://relp-server1.herokuapp.com/';
-//: 'https://relp.vsbabuj.repl.co/';
-const socket = io.connect(connectionString);
+    : 'https://relp-socket.me/';
 
+//: 'https://relp.vsbabuj.repl.co/';
+// https://relp-server1.herokuapp.com/
+
+const socket = io.connect(connectionString);
 export default socket;
