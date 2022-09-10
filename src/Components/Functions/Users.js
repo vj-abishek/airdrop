aad69fe3b69308a68555ec322e62292bf2543cd8
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2022-09-10 04:31:52 +0000

    wip: changes

--- src/Components/Functions/Users.js
+++ src/Components/Functions/Users.js
@@ -2,11 +2,12 @@ import io from 'socket.io-client';
 
 const connectionString = window.location.hostname === 'localhost'
     ? `http://${window.location.hostname}:8080`
-    : 'https://talented-bull-jumper.cyclic.app/'
+    : 'https://relp-server.up.railway.app/'
 
 //: 'https://relp.vsbabuj.repl.co/';
 // https://relp-server1.herokuapp.com/
 // https://relp-socket.me/
 // https://talented-bull-jumper.cyclic.app/
+// https://relp-server.up.railway.app/
 const socket = io.connect(connectionString);
 export default socket;
